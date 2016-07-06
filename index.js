7b36afeaaa860a257330ee93a553fe8cc383b821
Josh Washke <jwashke@icloud.com>
2016-07-05 20:22:12 -0600

    Created key up and key down event listeners for arrow keys

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
 var x = canvas.width / 2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var rightPressed = false;
+var leftPressed = false;
 
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
   ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095DD';
   ctx.fill();
   ctx.closePath();
 }
@@ -24,6 +37,24 @@ function draw() {
     dy = -dy
   x += dx;
   y += dy;
+}
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListneer('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.keyCode === 39)
+    rightPressed = true;
+  else if (e.keyCode === 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.keyCode === 39)
+    rightPressed = false;
+  else if (e.keyCode == 37)
+    leftPressed = false;
 }
 
 setInterval(draw, 10);
