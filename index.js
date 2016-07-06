f653ad929bbd032d83fd8fefe8ab79c5bd61f645
Josh Washke <jwashke@icloud.com>
2016-07-05 20:41:29 -0600

    Draw bricks on canvas

--- index.js
+++ index.js
@@ -11,6 +11,37 @@ var dy = -2;
 var ballRadius = 10;
 var rightPressed = false;
 var leftPressed = false;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for(c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for(r=0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0 };
+  }
+}
+
+function drawBricks() {
+  for (c = 0; c < brickColumnCount; c++) {
+    for (r = 0; r < brickRowCount; r++) {
+      var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+      var brickY = (r * (brickWidth + brickPadding)) + brickOffsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = '#0095DD';
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+}
 
 function drawBall() {
   ctx.beginPath();
@@ -30,6 +61,7 @@ function drawPaddle() {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
   drawBall();
   drawPaddle();
 
