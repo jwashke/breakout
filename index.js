68163edf6705ad233dae09e9fe2fcb013827db2a
Josh Washke <jwashke@icloud.com>
2016-07-05 20:47:07 -0600

    Add collision detection for bricks

--- index.js
+++ index.js
@@ -23,22 +23,24 @@ var bricks = [];
 for(c = 0; c < brickColumnCount; c++) {
   bricks[c] = [];
   for(r=0; r < brickRowCount; r++) {
-    bricks[c][r] = { x: 0, y: 0 };
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
   }
 }
 
 function drawBricks() {
   for (c = 0; c < brickColumnCount; c++) {
     for (r = 0; r < brickRowCount; r++) {
-      var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-      var brickY = (r * (brickWidth + brickPadding)) + brickOffsetTop;
-      bricks[c][r].x = brickX;
-      bricks[c][r].y = brickY;
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = '#0095DD';
-      ctx.fill();
-      ctx.closePath();
+      if (bricks[c][r].status === 1) {
+        var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+        var brickY = (r * (brickWidth + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = '#0095DD';
+        ctx.fill();
+        ctx.closePath();
+      }
     }
   }
 }
@@ -64,6 +66,7 @@ function draw() {
   drawBricks();
   drawBall();
   drawPaddle();
+  collisionDetection();
 
   if (rightPressed && paddleX < canvas.width - paddleWidth)
     paddleX += 7;
@@ -85,9 +88,6 @@ function draw() {
   y += dy;
 }
 
-document.addEventListener('keydown', keyDownHandler, false);
-document.addEventListener('keyup', keyUpHandler, false);
-
 function keyDownHandler(e) {
   if (e.keyCode === 39)
     rightPressed = true;
@@ -102,5 +102,24 @@ function keyUpHandler(e) {
   else if (e.keyCode == 37)
     leftPressed = false;
 }
+
+function collisionDetection() {
+  for(c = 0; c < brickColumnCount; c++) {
+    for (r = 0; r < brickRowCount; r++) {
+      var b = bricks[c][r];
+      if (b.status === 1) {
+        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+          dy = -dy
+          b.status = 0;
+        }
+      }
+    }
+  }
+}
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+
 
 setInterval(draw, 10);
